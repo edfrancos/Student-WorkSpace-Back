cd7baf0312f5a3cd10f88bda919aa4d28f04e9c6
Edwin Franco Sanchez <edfrancos@unal.edu.co>
2022-11-26 20:10:44 -0500

    Init project setup, Spring Framework added

---
+++ src/main/java/com/schedulemanager/StudentWorkSpaceBackApplication.java
@@ -0,0 +1,13 @@
+package com.schedulemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentWorkSpaceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentWorkSpaceBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schedulemanager/StudentWorkSpaceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedulemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentWorkSpaceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
